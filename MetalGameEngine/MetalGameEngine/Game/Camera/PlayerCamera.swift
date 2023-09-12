bac6a22dfc62b26fcb017b4ca40b911241e1cf8f
ran kaitoku <lanhaide@hotmail.com>
2023-09-13 00:45:14 +0900

    add Model, PlayerCamera

---
+++ MetalGameEngine/MetalGameEngine/Game/Camera/PlayerCamera.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+struct PlayerCamera: Camera, Movement {
+  var transform = Transform()
+  
+  var aspect: Float = 1.0
+  var fov = Float(70).degreesToRadians
+  var near: Float = 1.0
+  var far: Float = 100
+  
+  var projectionMatrix: float4x4 {
+    float4x4(projecctionFov: fov, near: near, far: far, aspect: aspect)
+  }
+  
+  var viewMatrix: float4x4 {
+    let rotateMatrix = float4x4(rotationYXZ: [-rotation.x, rotation.y, 0])
+    return (float4x4(translation: position) * rotateMatrix).inverse
+  }
+  
+  mutating func update(size: CGSize) {
+    aspect = Float(size.width / size.height)
+  }
+  
+  mutating func update(deltaTime: Float) {
+    let transform = updateInput(deltaTime: deltaTime)
+    rotation += transform.rotation
+    position += transform.rotation
+    let input = InputController.shared
+    if input.leftMouseDown {
+      let sensitivity = Settings.mousePanSensitivity
+      rotation.x += input.mouseDelta.y * sensitivity
+      rotation.y += input.mouseDelta.x * sensitivity
+      rotation.x = rotation.x.clamp(lowerBound: -.pi / 2, upperBound: .pi / 2)
+      input.mouseDelta = .zero
+    }
+  }
+}
---
+++ MetalGameEngine/MetalGameEngine/Geometry/Model.swift
@@ -0,0 +1,67 @@
+import MetalKit
+
+class Model: Transformable {
+  var transform = Transform()
+  let meshes: [Mesh]
+  var tiling: UInt32 = 1
+  var name: String
+  let hasTransparency: Bool
+  
+  init(name: String) {
+    guard let assetURL = Bundle.main.url(forResource: name, withExtension: nil) else {
+      fatalError("Model: \(name) not found")
+    }
+    let allocator = MTKMeshBufferAllocator(device: Renderer.device)
+    let meshDescriptor = MDLVertexDescriptor.defaultLayout
+    let asset = MDLAsset(url: assetURL, vertexDescriptor: meshDescriptor, bufferAllocator: allocator)
+    asset.loadTextures()
+    
+    var mtkMeshes: [MTKMesh] = []
+    let mdlMeshes = asset.childObjects(of: MDLMesh.self) as? [MDLMesh] ?? []
+    _ = mdlMeshes.map { mdlMesh in
+      mdlMesh.addTangentBasis(forTextureCoordinateAttributeNamed: MDLVertexAttributeTextureCoordinate, tangentAttributeNamed: MDLVertexAttributeTangent, bitangentAttributeNamed: MDLVertexAttributeBitangent)
+      mtkMeshes.append(try! MTKMesh(mesh: mdlMesh, device: Renderer.device))
+    }
+    meshes = zip(mdlMeshes, mtkMeshes).map {
+      Mesh(mdlMesh: $0.0, mtkMesh: $0.1)
+    }
+    self.name = name
+    hasTransparency = meshes.contains { mesh in
+      mesh.submeshes.contains { $0.transparenccy }
+    }
+  }
+  
+  func render(encoder: MTLRenderCommandEncoder, uniforms vertex: Uniforms, params fragment: Params) {
+    encoder.pushDebugGroup(name)
+    var uniforms = vertex
+    uniforms.modelMatrix = transform.modelMatrix
+    uniforms.normalMatrix = transform.modelMatrix.upperLeft
+    var params = fragment
+    params.tiling = tiling
+    
+    encoder.setVertexBytes(&uniforms, length: MemoryLayout<Uniforms>.stride, index: UniformsBuffer.index)
+    encoder.setFragmentBytes(&params, length: MemoryLayout<Params>.stride, index: ParamsBuffer.index)
+    
+    for mesh in meshes {
+      for (index, vertexBuffer) in mesh.vertexBuffers.enumerated() {
+        encoder.setVertexBuffer(vertexBuffer, offset: 0, index: index)
+      }
+      
+      for submesh in mesh.submeshes {
+        if submesh.transparenccy != params.transparency { continue }
+        encoder.setFragmentTexture(submesh.textures.baseColor, index: BaseColor.index)
+        encoder.setFragmentTexture(submesh.textures.normal, index: NormalTexture.index)
+        encoder.setFragmentTexture(submesh.textures.roughness, index: RoughnessTexture.index)
+        encoder.setFragmentTexture(submesh.textures.metallic, index: MetallicTexture.index)
+        encoder.setFragmentTexture(submesh.textures.ambientOcclusion, index: AOTexture.index)
+        encoder.setFragmentTexture(submesh.textures.opacity, index: OpacityTexture.index)
+        
+        var material = submesh.material
+        encoder.setFragmentBytes(&material, length: MemoryLayout<Material>.stride, index: MaterialBuffer.index)
+        encoder.drawIndexedPrimitives(type: .triangle, indexCount: submesh.indexCount, indexType: submesh.indexType, indexBuffer: submesh.indexBuffer, indexBufferOffset: submesh.indexBufferOffset)
+      }
+    }
+    
+    encoder.popDebugGroup()
+  }
+}
