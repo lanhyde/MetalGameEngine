56f312deccea6bf2ab8910db28558127b0c56fd2
ran kaitoku <lanhaide@hotmail.com>
2023-09-13 22:33:30 +0900

    add shadow camera

--- MetalGameEngine/MetalGameEngine/Game/GameController.swift
+++ MetalGameEngine/MetalGameEngine/Game/GameController.swift
@@ -1,10 +1,34 @@
-import Foundation
+import MetalKit
 
 class GameController: NSObject {
   var options = Options()
+  var renderer: Renderer
+  var scene: GameScene
+  var fps: Double = 0
+  var deltaTime: Double = 0
+  var lastTime: Double = CFAbsoluteTimeGetCurrent()
   
-  init(options: Options) {
+  init(metalView: MTKView, options: Options) {
+    renderer = Renderer(metalView: metalView, options: options)
+    scene = GameScene()
     super.init()
     self.options = options
+    metalView.delegate = self
+    fps = Double(metalView.preferredFramesPerSecond)
+  }
+}
+
+extension GameController: MTKViewDelegate {
+  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    scene.update(size: size)
+    renderer.mtkView(view, drawableSizeWillChange: size)
+  }
+  
+  func draw(in view: MTKView) {
+    let currentTime = CFAbsoluteTimeGetCurrent()
+    let deltaTime = (currentTime - lastTime)
+    lastTime = currentTime
+    scene.update(deltaTime: Float(deltaTime))
+    renderer.draw(scene: scene, in: view)
   }
 }
